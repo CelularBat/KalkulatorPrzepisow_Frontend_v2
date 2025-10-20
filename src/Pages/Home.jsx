80a63bcd43c48af033cac448034d12ba78445d55
CelularBat <105959781+CelularBat@users.noreply.github.com>
2025-10-20 14:49:32 +0200

    upd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ function Home({}) {
                 <h2>Witaj w kalkulatorze przepisów !</h2>
                 <div className='header-line'/>
                 <ul>
-                    <li>Aplikacji dla osób o rygorystycznych dietach i tych liczacych kalorie.</li>
+                    <li>Aplikacji dla osób stosujących rygorytyczne diety i tych liczacych kalorie.</li>
                     <li>Załóż konto, dodaj do bazy produkty z lodówki, albo korzystaj już z tych dodanych przez innych użytkowników</li>
                     <li>Podziel się własnymi przepisami, a aplikacja automatycznie obliczy ich wartośc odżywczą</li>
                     <li>W przyszłości integracja z bazą Open Food Facts!</li>
